09f6a6e935fbaea606e8e884a2d2fb1b952c642c
Frank Alkema <frank.alkema@icloud.com>
2018-11-05 16:13:02 +0100

    added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# custom routes
+  get "spaces", to: "space#index", as: :list
+  get "spaces/:id", to: "space#show", as: :space
+  get "user/:id", to: "user#show"
+  get "dashboard", to: "user#dashboard"
+  get "spaces/new", to: "space#new"
+  post "space", to: "space#create"
+  get "space/:id/edit", to: 'space#edit'
+  patch "space/:id", to:'space#update'
+  delete "space/:id", to: 'space#destroy'
 end
