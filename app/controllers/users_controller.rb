26bfa73e3702e1a5d1d31b68d1d2862dd8754eae
Frank Alkema <frank.alkema@icloud.com>
2018-11-05 18:55:25 +0100

    made the dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,15 @@ class UsersController < ApplicationController
 
   def dashboard
 
+   if user_signed_in?
+    @user = User.find(current_user.id)
+    @spaces = Space.where(user_id: current_user.id)
+
+
+    # @user =User.find(params[:id])
+    # @spaces = Space.where(user_id:params[:id])
+  else
+    redirect_to new_user_session_path
   end
+end
 end
