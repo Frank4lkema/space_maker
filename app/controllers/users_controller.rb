df000295487ff7e2e2b1928e180f6afa2b0e3298
Frank Alkema <frank.alkema@icloud.com>
2018-11-05 18:31:25 +0100

    added the dashboard method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @spaces = Space.where(user_id: params[:id])
+
+  end
+
+  def dashboard
+
   end
 end
