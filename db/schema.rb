4fc7473535aa5649a0127366cef0e2bf86e8e244
Frank Alkema <frank.alkema@icloud.com>
2018-11-06 15:04:24 +0100

    added the pictures model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_101423) do
+ActiveRecord::Schema.define(version: 2018_11_06_140002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2018_11_06_101423) do
     t.datetime "updated_at", null: false
     t.index ["space_id"], name: "index_bookings_on_space_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "url"
+    t.bigint "space_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["space_id"], name: "index_pictures_on_space_id"
   end
 
   create_table "spaces", force: :cascade do |t|
@@ -56,5 +64,6 @@ ActiveRecord::Schema.define(version: 2018_11_06_101423) do
 
   add_foreign_key "bookings", "spaces"
   add_foreign_key "bookings", "users"
+  add_foreign_key "pictures", "spaces"
   add_foreign_key "spaces", "users"
 end
