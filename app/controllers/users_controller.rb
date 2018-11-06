6fb29bd7f37553cb1972045c5203ec695324afa4
Jordy Kleberg <jpkleberg@gmail.com>
2018-11-06 17:44:19 +0100

    Added authorization to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @spaces = Space.where(user_id: params[:id])
+    authorize @user
 
+    @spaces = Space.where(user_id: params[:id])
   end
 
   def dashboard
-
-   if user_signed_in?
-    @user = User.find(current_user.id)
-    @spaces = Space.where(user_id: current_user)
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      authorize @user
+      @spaces = Space.where(user_id: current_user)
     else
-    redirect_to new_user_session_path
+      redirect_to new_user_session_path
     end
   end
 end
