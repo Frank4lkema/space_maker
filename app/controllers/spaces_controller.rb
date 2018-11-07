38074c7f33fafc5659171192c04fd151ee239c20
Jordy Kleberg <jpkleberg@gmail.com>
2018-11-07 13:08:27 +0100

    Fixed authorization issues when not logged in

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,5 @@
 class SpacesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @spaces = policy_scope(Space)
 
